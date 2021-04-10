3a91c3dde3feff18972d83831a7b3dc6c72fb61a
jacobspeicher <speicherjacob@gmail.com>
2021-04-10 11:15:14 -0500

    new initial commit, moved from C++ to C#

---
+++ speiCHR WPF/speiCHR WPF/Files/FileProcessor.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace speiCHR.Files
+{
+    class FileProcessor
+    {
+        BinaryReader reader;
+        public FileProcessor()
+        {
+        }
+
+        static FileProcessor()
+        {
+
+        }
+        public string ReadFiles(string file)
+        {
+            reader = new BinaryReader(File.Open(file, FileMode.Open));
+
+            byte[] bits = reader.ReadBytes(16);
+
+            string output = "";
+            for(int i = 0; i < 8; ++i)
+            {
+                string row = "";
+                for(int j = 0; j < 8; ++j)
+                {
+                    int lo_bit = bits[i] & 1;
+                    int hi_bit = bits[i + 8] & 1;
+                    bits[i] = (byte)(bits[i] >> 1);
+                    bits[i + 8] = (byte)(bits[i + 8] >> 1);
+                    row = lo_bit.ToString() + hi_bit.ToString() + " " + row;
+                }
+                output += row + "\n";
+            }
+
+            return output;
+        }
+    }
+}
---
+++ speiCHR WPF/speiCHR WPF/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using speiCHR.Files;
+
+namespace speiCHR
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private FileProcessor fp;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            fp = new FileProcessor();
+        }
+
+        private void OpenFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            dlg.DefaultExt = ".chr";
+            dlg.Filter = "CHR Files (.chr)|*.chr";
+
+            // Show the open file dialog box
+            Nullable<bool> result = dlg.ShowDialog();
+
+            // Process open file dialog box results
+            if(result == true)
+            {
+                // Open document
+                string filename = dlg.FileName;
+                string output = fp.ReadFiles(filename);
+                MessageBox.Show(output);
+            }
+        }
+    }
+}
