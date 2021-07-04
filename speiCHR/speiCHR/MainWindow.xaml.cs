c324e044cdaa68a1eb0113569deccec8a7fe352f
jacobspeicher <speicherjacob@gmail.com>
2021-07-04 10:25:17 -0500

    Added an 8x8 sprite of pixels, put the palette on the screen

--- speiCHR/speiCHR/MainWindow.xaml.cs
+++ speiCHR/speiCHR/MainWindow.xaml.cs
@@ -24,13 +24,15 @@ namespace speiCHR
         private FileProcessor fp;
         private CHRFile chrFile;
         private Palette[] palettes = new Palette[4];
+        private Brush curBrush;
 
         public MainWindow()
         {
             InitializeComponent();
-            chrFile = new CHRFile();
-            fp = new FileProcessor(chrFile);
-            palettes[0] = new Palette();
+            speiChrSetup();
+            spriteGridSetup();
+            paletteGridSetup();
+            
             CommandBinding OpenCmdBinding = new CommandBinding(
                 ApplicationCommands.Open,
                 OpenCmdExecuted,
@@ -45,6 +47,77 @@ namespace speiCHR
             tbl_FileName.SetBinding(TextBlock.TextProperty, FileNameBinding);
 
             this.CommandBindings.Add(OpenCmdBinding);
+        }
+
+        private void speiChrSetup()
+        {
+            chrFile = new CHRFile();
+            fp = new FileProcessor(chrFile);
+            palettes[0] = new Palette();
+            curBrush = palettes[0].Colors[0];
+        }
+
+        private void spriteGridSetup()
+        {
+            SpriteGrid.Height = 200;
+            SpriteGrid.Width = 200;
+
+            for (int i = 0; i < 8; ++i)
+            {
+                ColumnDefinition colDef = new ColumnDefinition();
+                RowDefinition rowDef = new RowDefinition();
+
+                colDef.Width = new GridLength(25);
+                rowDef.Height = new GridLength(25);
+
+                SpriteGrid.ColumnDefinitions.Add(colDef);
+                SpriteGrid.RowDefinitions.Add(rowDef);
+            }
+
+            for (int i = 0; i < 8; ++i)
+            {
+                for (int j = 0; j < 8; ++j)
+                {
+                    Rectangle px = new Rectangle();
+                    px.Fill = Brushes.Black;
+                    px.Height = 25;
+                    px.Width = 25;
+                    px.MouseLeftButtonDown += new MouseButtonEventHandler(FillPixel);
+                    Grid.SetRow(px, i);
+                    Grid.SetColumn(px, j);
+
+                    SpriteGrid.Children.Add(px);
+                }
+            }
+        }
+
+        private void paletteGridSetup()
+        {
+            PaletteGrid.Height = 25;
+            PaletteGrid.Width = 100;
+
+            for(int i = 0; i < 4; ++i)
+            {
+                ColumnDefinition colDef = new ColumnDefinition();
+                colDef.Width = new GridLength(25);
+                PaletteGrid.ColumnDefinitions.Add(colDef);
+            }
+            RowDefinition rowDef = new RowDefinition();
+            rowDef.Height = new GridLength(25);
+            PaletteGrid.RowDefinitions.Add(rowDef);
+
+            for(int i = 0; i < 4; ++i)
+            {
+                Rectangle rect = new Rectangle();
+                rect.Fill = palettes[0].Colors[i];
+                rect.Height = 25;
+                rect.Width = 25;
+                rect.MouseLeftButtonDown += new MouseButtonEventHandler(SelectPaletteColor);
+                Grid.SetRow(rect, 0);
+                Grid.SetColumn(rect, i);
+
+                PaletteGrid.Children.Add(rect);
+            }
         }
 
         void OpenCmdExecuted(object target, ExecutedRoutedEventArgs e)
@@ -63,17 +136,22 @@ namespace speiCHR
                 string filename = dlg.FileName;
                 string output = fp.ReadFiles(filename);
                 MessageBox.Show(output);
-                DrawImage();
+                DrawSprite();
             }
         }
 
-        void DrawImage()
+        private void DrawSprite()
         {
-            List<Color> colors = new List<Color>();
-            colors.Add(Colors.Black);
-            colors.Add(Colors.Red);
-            colors.Add(Colors.Green);
-            colors.Add(Colors.Blue);
+            for(int i = 0; i < 8; ++i)
+            {
+                for(int j = 0; j < 8; ++j)
+                {
+                    int pxPos = i * 8 + j;
+                    int paletteColor = chrFile.Pixels[i, 7 - j];
+                    Rectangle px = (Rectangle)SpriteGrid.Children[pxPos];
+                    px.Fill = palettes[0].Colors[paletteColor];
+                }
+            }
         }
 
         void OpenCmdCanExecute(object sender, CanExecuteRoutedEventArgs e)
@@ -81,17 +159,17 @@ namespace speiCHR
             e.CanExecute = true;
         }
 
-        void ButtonClick(object sender, RoutedEventArgs e)
-        {
-            Button btn = (Button)sender;
-            btn.Background = Brushes.Pink;
-        }
-
-        void RectClick(object sender, RoutedEventArgs e)
+        void FillPixel(object sender, RoutedEventArgs e)
         {
             Rectangle rect = (Rectangle)sender;
 
-            rect.Fill = palettes[0].Colors[1];
+            rect.Fill = curBrush;
+        }
+
+        void SelectPaletteColor(object sender, RoutedEventArgs e)
+        {
+            Rectangle rect = (Rectangle)sender;
+            curBrush = rect.Fill;
         }
     }
 }
